adcf7eaa81d1ed802ab0346521fe7079c1301318
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-03 22:43:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,9 @@ function keepGoing(array, changeValue, skipValue) {
 }
 
 function findBy(array, findFn) {
-
+  for (let i = 0; i < array.length; i++) {
+    if (findFn(array[i])) {
+      return array[i]
+    }
+  }
 }
