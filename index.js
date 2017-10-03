a4690ddeb692398518ba8b500e312a9c92868a5c
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-03 22:37:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,11 @@ function breakOut(array, changeValue, stopValue) {
 }
 
 function keepGoing(array, changeValue, skipValue) {
-
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) {
+      break;
+    }
+    array[i] = changeValue;
+  }
+  return array;
 }
