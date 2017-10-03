24be6a4d98e8301a30a52e9f895604f5a7369a4e
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-03 22:37:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function breakOut(array, changeValue, stopValue) {
 
 function keepGoing(array, changeValue, skipValue) {
   for (let i = 0; i < array.length; i++) {
+    array[i] = changeValue;
     if (array[i] === skipValue) {
       break;
     }
-    array[i] = changeValue;
   }
   return array;
 }
