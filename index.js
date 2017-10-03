a886f8ea735de0c6adc3ebe53b30a10da6a63aa3
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-03 22:38:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function keepGoing(array, changeValue, skipValue) {
   for (let i = 0; i < array.length; i++) {
     array[i] = changeValue;
     if (array[i] === skipValue) {
+      array[i] = skipValue
       break;
     }
   }
